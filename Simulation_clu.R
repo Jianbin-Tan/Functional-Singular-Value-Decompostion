cfad9dd5739dbdbef3e001d8f441daa31b638492
JB <tanjb6@mail2.sysu.edu.cn>
2025-12-16 15:08:56 -0500

    Update Simulation_clu.R

--- Simulation_clu.R
+++ Simulation_clu.R
@@ -1,5 +1,6 @@
 # Packages and functions
 library(snowfall)
+library(doRNG)
 source("Function.R")
 source("Jame_FC.R")
 
@@ -9,15 +10,14 @@ norm <- rowMeans(sapply(1:200, function(k){
 }))
 
 # Simulation
-cl <- parallel:: makeCluster(parallel::detectCores() - 1)
+cl <- 40
 invisible(gc())
 doParallel:: registerDoParallel(cl)
 
-Result <- foreach(seed = seq(1, 10000, length.out = 2)) %dopar% {
+Result <- foreach(seed = seq(1, 10000, length.out = 100)) %dorng% {
   sim_func_clu(basis_num = basis_num, n = n,
                obs_point = obs_point, norm = norm,
                rat = rat, seed = seed)
 }
-
 
 save(Result, file = paste0("Result/Result_", n, "_", obs_point, "_", "clu", ".rda"), version = 2)
